e7cc84f1e848f6895583a106609fbe5f6fdcaccf
Heshan Rathnapala <43751413+hrathnapala@users.noreply.github.com>
2025-03-15 22:18:06 +0530

    user creation db save part

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,9 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.get('/', (req,res) => {
     res.send('Server is ready')
 })
