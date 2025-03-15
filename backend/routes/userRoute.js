9798d0ced9d646c557e2eba76c70a752e8131567
Heshan Rathnapala <43751413+hrathnapala@users.noreply.github.com>
2025-03-15 21:26:47 +0530

    Added custom error middleware function

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,6 +5,8 @@ import asyncHandler from 'express-async-handler';
 // @ROUTE POST api/users/auth
 // @ACCESS public
 const authUser = asyncHandler(async (req,res) => {
+    res.status(401);
+    throw new Error('Something went wrong');
     res.status(200).json({message: "Auth User"});
 })
 
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
+// middleware
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
+
 import { authUser } from './routes/userRoute.js';
 
 dotenv.config();
@@ -13,6 +17,9 @@ app.get('/', (req,res) => {
 })
 
 app.use("/api/users/",authUser);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT,() => {
     console.log(`Server started on port ${PORT}`)
