ba2e5fcfb4ad7c8cc16e2c14194a9f22b9eb97a4
Heshan Rathnapala <43751413+hrathnapala@users.noreply.github.com>
2025-03-16 12:38:22 +0530

    backend completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,21 @@ import User from '../models/userModel.js';
 // @ROUTE POST api/users/auth
 // @ACCESS public
 const authUser = asyncHandler(async (req,res) => {
-    res.status(200).json({message: "Auth User"});
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email});
+
+    if(user && (await user.matchPasswords(password))) {
+        generateToken(res,user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 })
 
 // @DESC Register a new user
@@ -43,21 +57,54 @@ const registerUser = asyncHandler(async (req,res) => {
 // @ROUTE POST api/users/logout
 // @ACCESS public
 const logoutUser = asyncHandler(async (req,res) => {
-    res.status(200).json({message: "Logout User"});
+
+    res.cookie('jwt','',{
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    
+    res.status(200).json({message: "User logged out"});
 })
 
 // @DESC Get user profile
 // @ROUTE POST api/users/profile
 // @ACCESS private
 const getUserProfile = asyncHandler(async (req,res) => {
-    res.status(200).json({message: "User Profile"});
+    console.log(req.user);
+    
+    const user = {
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email
+    }
+    
+    res.status(200).json(user);
 })
 
 // @DESC Update user profile
 // @ROUTE PUT api/users/profile
 // @ACCESS private
 const updateUserProfile = asyncHandler(async (req,res) => {
-    res.status(200).json({message: "Update User Profile"});
+    const user = await User.findById(req.user._id);
+
+    if(user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if(req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updateUser = await user.save();
+        res.status(200).json({
+            _id: updateUser._id,
+            name: updateUser.name,
+            email: updateUser.email
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 })
 
 export {authUser, registerUser, logoutUser, getUserProfile, updateUserProfile}
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -17,7 +17,7 @@ const userSchema = mongoose.Schema({
     }
 },{timestamps: true});
 
-userSchema.pre('save',async function name(next) {
+userSchema.pre('save',async function (next) {
     if(!this.isModified('password')) {
         next()
     } 
@@ -25,6 +25,10 @@ userSchema.pre('save',async function name(next) {
     const salt = await bcrypt.genSalt(10,)
     this.password = await bcrypt.hash(this.password,salt);
 });
+
+userSchema.methods.matchPasswords = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword,this.password);
+}
 
 const User = mongoose.model('User',userSchema);
 
